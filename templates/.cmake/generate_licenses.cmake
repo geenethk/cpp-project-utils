315534b22f1d7f521787a9c66a71dd3414d6d901
geenethk <babycypher4@gmail.com>
2026-06-04 16:51:59 +0530

    Fixed license combination problem

--- templates/.cmake/generate_licenses.cmake
+++ templates/.cmake/generate_licenses.cmake
@@ -1,31 +1,32 @@
-if(NOT DEFINED VCPKG_SHARE_DIR OR NOT DEFINED LICENSE_OUTPUT)
-    message(FATAL_ERROR "Missing variables")
+if(NOT DEFINED VCPKG_SHARE_DIR)
+    message(FATAL_ERROR "VCPKG_SHARE_DIR is not defined")
 endif()
 
-file(WRITE "${LICENSE_OUTPUT}"
-"Third Party Licenses
+if(NOT DEFINED LICENSE_OUTPUT)
+    message(FATAL_ERROR "LICENSE_OUTPUT is not defined")
+endif()
+
+set(OUTPUT_TEXT
+    "Third Party Licenses
 ====================
 
-This file contains license notices for third-party libraries.
-")
+This file contains license notices for third-party libraries.")
 
-file(GLOB PORT_DIRS "${VCPKG_SHARE_DIR}/*")
+file(GLOB PORT_DIRS LIST_DIRECTORIES true "${VCPKG_SHARE_DIR}/*")
 
 foreach(PORT_DIR ${PORT_DIRS})
-    if(EXISTS "${PORT_DIR}/copyright")
-
+    if(IS_DIRECTORY "${PORT_DIR}" AND EXISTS "${PORT_DIR}/copyright")
         get_filename_component(PORT_NAME "${PORT_DIR}" NAME)
-
-        file(APPEND "${LICENSE_OUTPUT}"
-"\n\n============================================================\n")
-        file(APPEND "${LICENSE_OUTPUT}" "${PORT_NAME}\n")
-        file(APPEND "${LICENSE_OUTPUT}"
-"============================================================\n\n")
-
         file(READ "${PORT_DIR}/copyright" LICENSE_TEXT)
 
-        file(APPEND "${LICENSE_OUTPUT}" "${LICENSE_TEXT}")
+        set(OUTPUT_TEXT "${OUTPUT_TEXT}
+============================================================
+${PORT_NAME}
+============================================================
+${LICENSE_TEXT}")
     endif()
 endforeach()
+
+file(WRITE "${LICENSE_OUTPUT}" "${OUTPUT_TEXT}")
 
 message(STATUS "Generated: ${LICENSE_OUTPUT}")
